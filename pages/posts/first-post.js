46f2554047f0ded0823b38f556b30125f238ba8c
Michaela Wagner <mickywagner@gmail.com>
2020-10-01 14:21:06 -0600

    Add global styles to app page

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,10 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
             <Head>
                 <title>First Post</title>
 
@@ -12,6 +13,6 @@ export default function FirstPost() {
             <h2>
                 <Link href="/">Back to Home</Link>
             </h2>
-        </>
+        </Layout>
     )
 }
