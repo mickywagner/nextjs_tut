fb19e5cbda48b1ccdfc38abd32abc1383be2ac74
Michaela Wagner <mickywagner@gmail.com>
2020-10-01 15:58:17 -0600

    Add StaticProps to display blog posts

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
-import utilStyes from "../styles/utils.module.css";
+import utilStyles from "../styles/utils.module.css";
 
-export default function Home() {
+import { getSortedPostsData } from '../lib/posts'
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyes.headingMd}>
+      <section className={utilStyles.headingMd}>
         <p>Hi, I'm Michaela. I'm a web developer looking for a job. I would love to work
           on Frontend or Backend projects. I use Javascript/React and now Gatsby and next
           as well as Node/Express/MongoDB.
@@ -18,6 +20,29 @@ export default function Home() {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
-  );
+  )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
